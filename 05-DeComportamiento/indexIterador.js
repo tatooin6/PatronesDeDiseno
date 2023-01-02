0fb535a60afc72bfe00bf30e4229cb01c49c823f
Antonio Pantoja <tato_fos@hotmail.com>
2023-01-01 23:59:41 -0400

    31. Iterador

---
+++ 05-DeComportamiento/indexIterador.js
@@ -0,0 +1,20 @@
+function* iterador(col) {
+  nextIndex = 0;
+
+  while(nextIndex < col.length) {
+    yield col[nextIndex++];
+  }
+}
+
+const x = [1,2,3,4,5,6,7,8,9]
+const gen = iterador(x)
+console.log(gen.next())
+console.log(gen.next())
+console.log(gen.next())
+console.log(gen.next())
+console.log(gen.next())
+console.log(gen.next())
+console.log(gen.next())
+console.log(gen.next())
+console.log(gen.next())
+console.log(gen.next())
